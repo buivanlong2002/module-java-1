bdcc8db369bb69953ddc81c48606cc71f84a5948
MarcoHoang <hoang111uuu@gmail.com>
2025-04-08 17:24:05 +0700

    Point2D

---
+++ Inherit-Java/Exercise6/Point2D-Point3D/src/Point3D.java
@@ -0,0 +1,25 @@
+public class Point3D extends Point2D {
+    private float z;
+    public Point3D() {}
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+    public float getZ() {
+        return z;
+    }
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public float[] getXYZ() {
+        return new float[]{getX(), getY(), z};
+    }
+    public void setXYZ(float x, float y, float z) {
+        setX(x);
+        setY(y);
+        this.z = z;
+    }
+    public String toString() {
+        return "Point3D (" + getX() + ", " + getY() + ", " + z + ")";
+    }
+}
