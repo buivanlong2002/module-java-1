04b4e74947a1cf72e06b4fd7c45ffc7d82a0e9da
MarcoHoang <hoang111uuu@gmail.com>
2025-04-08 10:57:40 +0700

    Student

---
+++ Access-Modifier-Static-Method/Exercise5/Student-Java/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Hoang");
+        s1.setClasses("C00");
+        s1.display();
+        Student s2 = new Student();
+        s2.setName("Marco");
+        s2.setClasses("C01");
+        s2.display();
+    }
+}
