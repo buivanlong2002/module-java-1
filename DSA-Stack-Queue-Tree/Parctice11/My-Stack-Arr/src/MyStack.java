e95d060738321cfb49d83e869b0bb1736150a0d1
MarcoHoang <hoang111uuu@gmail.com>
2025-04-17 23:48:39 +0700

    StackArr

---
+++ DSA-Stack-Queue-Tree/Parctice11/My-Stack-Arr/src/MyStack.java
@@ -0,0 +1,38 @@
+public class MyStack {
+    private int[] queueArr;
+    private int size;
+    private int index = 0;
+    public MyStack(int size) {
+        this.size = size;
+        queueArr = new int[size];
+    }
+    public  void push(int x) {
+        if (isFull()) {
+            throw new RuntimeException("Stack is full!");
+        }
+        queueArr[index] = x;
+        index++;
+    }
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty!");
+        }
+        return queueArr[--index];
+    }
+    public int size() {
+        return index;
+    }
+
+    public boolean isEmpty() {
+        if (index == 0) {
+            return true;
+        }
+        return false;
+    }
+    public boolean isFull() {
+        if (index == size) {
+            return true;
+        }
+        return false;
+    }
+}
