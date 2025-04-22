d36c9267638d771b152a0990918475ee9aea4cc2
MarcoHoang <hoang111uuu@gmail.com>
2025-04-22 15:39:13 +0700

    StringConsecutive

---
+++ Search-Algorithm/Exercise13/Longest-Ascending-String-Consecutive/src/CalculateComplexity.java
@@ -0,0 +1,10 @@
+public class CalculateComplexity {
+    public static void main(String[] args) {
+//        - input.length() = n
+//        - Line 10 : O(1)
+//        - Line 16 (for) : O(n)
+//        - Line 12 (for(for)) : 0(n2)
+//        - Line 28 (for) : O(n)
+//        -> Complexity : O(n2)
+    }
+}
