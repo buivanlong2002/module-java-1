49bf1a260087e9525f7c9f7718750075d036bc10
MarcoHoang <hoang111uuu@gmail.com>
2025-04-28 08:17:35 +0700

    Minitest7

--- Minitest/Minitest7/src/controller/CustomerNameComparator.java
+++ Minitest/Minitest7/src/controller/CustomerNameComparator.java
@@ -2,7 +2,15 @@ package controller;
 
 import model.Order;
 
-public class CustomerNameComparator implements java.util.Comparator<Order>{
+public class CustomerNameComparator implements java.util.Comparator<Order> {
     @Override
-    public int compare(Order o1, Order o2) { return o1.getCustomerName().compareToIgnoreCase(o2.getCustomerName()); }
+    public int compare(Order o1, Order o2) {
+        if (o1 == null || o1.getCustomerName() == null) {
+            return (o2 == null || o2.getCustomerName() == null) ? 0 : -1;
+        }
+        if (o2 == null || o2.getCustomerName() == null) {
+            return 1;
+        }
+        return o1.getCustomerName().compareToIgnoreCase(o2.getCustomerName());
+    }
 }
--- Minitest/Minitest7/src/controller/TotalPriceComparator.java
+++ Minitest/Minitest7/src/controller/TotalPriceComparator.java
@@ -2,9 +2,15 @@ package controller;
 
 import model.Order;
 
-public class TotalPriceComparator implements java.util.Comparator<Order>{
+public class TotalPriceComparator implements java.util.Comparator<Order> {
     @Override
     public int compare(Order o1, Order o2) {
+        if (o1 == null) {
+            return (o2 == null) ? 0 : -1;
+        }
+        if (o2 == null) {
+            return 1;
+        }
         return Double.compare(o1.calculateTotalPrice(), o2.calculateTotalPrice());
     }
 }
