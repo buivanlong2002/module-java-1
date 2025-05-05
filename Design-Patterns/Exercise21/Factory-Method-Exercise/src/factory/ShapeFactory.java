7e30203918e59c0f38aa62e012ddbc87a3631447
MarcoHoang <hoang111uuu@gmail.com>
2025-05-06 00:27:56 +0700

    FactoryMethod

---
+++ Design-Patterns/Exercise21/Factory-Method-Exercise/src/factory/ShapeFactory.java
@@ -0,0 +1,23 @@
+package factory;
+
+import shape.*;
+
+public class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+
+        switch (shapeType.toLowerCase()) {
+            case "circle":
+                return new Circle();
+            case "square":
+                return new Square();
+            case "rectangle":
+                return new Rectangle();
+            default:
+                return null;
+        }
+    }
+}
